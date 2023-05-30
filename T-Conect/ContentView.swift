b3eaad5ce5178b7be9724786e7e4b73472801635
Kevin Hernandez <kmhs-18@hotmail.com>
2023-05-30 18:46:52 -0300

    Initial Commit

---
+++ T-Conect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  T-Conect
+//
+//  Created by Kevin Hernandez on 30/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ T-Conect/T_ConectApp.swift
@@ -0,0 +1,17 @@
+//
+//  T_ConectApp.swift
+//  T-Conect
+//
+//  Created by Kevin Hernandez on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct T_ConectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
